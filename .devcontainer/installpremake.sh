8b0ca3f3473847e548e051d00eafe6fc36c492e4
reuner <christopher.reuner@gmail.com>
2025-07-16 19:53:25 +0000

    Setup Buildsystem

---
+++ .devcontainer/installpremake.sh
@@ -0,0 +1,7 @@
+cd /tmp
+
+wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-linux.tar.gz
+
+tar -xvzf premake-5.0.0-alpha16-linux.tar.gz
+
+sudo mv premake5 /usr/local/bin/
---
+++ premake5.lua
@@ -0,0 +1,30 @@
+workspace "Pong"
+   configurations { "Debug", "Release" }
+   startproject "App"
+
+project "App"
+   location "build"
+   kind "WindowedApp"
+   language "C++"
+   cppdialect "C+++23"
+   staticruntime "on"
+   warnings "Extra"
+
+   targetdir ("bin/%{cfg.buildcfg}")
+   objdir ("bin-int/%{cfg.buildcfg}")
+
+   files { "src/**.cpp", "include/**.h" }
+
+   includedirs { "include" }
+
+   filter "system:linux"
+      systemversion "latest"
+      links { "pthread" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
