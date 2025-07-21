52bb1eae79a55bbf964d037876eb21d283b017d4
reuner <christopher.reuner@gmail.com>
2025-07-21 17:22:23 +0000

    added sourcesdk as dependencie

--- buildshader.sh
+++ buildshader.sh
@@ -1,3 +1,3 @@
 mkdir build/shader
-./extern/vulkansdk/x86_64/bin/glslc src/shader/shader.frag -o build/shader/frag.spv
-./extern/vulkansdk/x86_64/bin/glslc src/shader/shader.vert -o build/shader/vert.spv
+./build/external/vulkansdk/x86_64/bin/glslc src/shader/shader.frag -o build/shader/frag.spv
+./build/external/vulkansdk/x86_64/bin/glslc src/shader/shader.vert -o build/shader/vert.spv
