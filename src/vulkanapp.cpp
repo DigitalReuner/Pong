6e28264a6ca9867b39fa14f4f80c3021f952a7b6
DigitalReuner <reuner.christopher@gmail.com>
2025-07-22 11:26:33 +0200

    added stb_image

--- src/vulkanapp.cpp
+++ src/vulkanapp.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <chrono>
 #include "glm/gtc/matrix_transform.hpp"
+#include "stb_image.h"
 
 const std::vector<Vertex> vertices = {
     {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
